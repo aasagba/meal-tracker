308c19af0442bb62596103d462aa9471d74c9dc9
Adrian Asagba <adrian@littleforest.co.uk>
2016-07-20 16:34:27 +0100

    first commit

---
+++ www/js/controllers/authentication.js
@@ -0,0 +1,49 @@
+var app = angular.module('mealtrack.controllers.authentication', []);
+
+/*********************************************************************
+ * LoginCtrl
+ *********************************************************************/
+app.controller('LoginCtrl', function ($scope, $state, AuthService) {
+
+	$scope.formData = {
+		"email": "",
+		"password": ""
+	};
+
+	$scope.login = function (form) {
+		console.log("LoginCtrl::login");
+    if(form.$valid) {
+      AuthService.login($scope.formData.email, $scope.formData.password)
+        .then(function () {
+          $state.go("tab.meals");
+        });
+    }
+
+	};
+
+});
+
+/*********************************************************************
+ * SignupCtrl
+ *********************************************************************/
+app.controller('SignupCtrl', function ($scope, $state, AuthService) {
+
+	$scope.formData = {
+		"name": "",
+		"email": "",
+		"password": ""
+	};
+
+	$scope.signup = function (form) {
+    if(form.$valid) {
+      console.log("SignupCtrl::signup");
+      AuthService.signup($scope.formData.email,
+        $scope.formData.name,
+        $scope.formData.password)
+        .then(function () {
+          $state.go("tab.meals");
+        });
+    }
+	};
+
+});
